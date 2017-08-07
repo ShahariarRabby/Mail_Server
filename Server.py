33e16391906622002ae621ab83d0f907725c3c34
Shahariar Rabby <ShahariarRabby@gmail.com>
2017-08-08 00:09:10 +0600

    Description added: Check new mail in every 15 sec working

--- Server.py
+++ Server.py
@@ -2,6 +2,8 @@
 # ! /usr/bin/python
 __author__ = 'Shahariar Rabby'
 
+# This will connect with your mail server and check new mail for every 15 sec
+# When new massage come it will play sound and notify user
 
 from ReceiveMail import *
 server()

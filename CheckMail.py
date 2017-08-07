6f1491c805566c7123b52651617702f53a860fff
Shahariar Rabby <ShahariarRabby@gmail.com>
2017-08-08 00:11:16 +0600

    This will check mail from gmail server, read mail working

--- CheckMail.py
+++ CheckMail.py
@@ -2,6 +2,7 @@
 # ! /usr/bin/python
 __author__ = 'Shahariar Rabby'
 
+#This file will check your mail from your gmail server
 
 from ReceiveMail import *
 read = int(raw_input("Enter how many message you want to read : "))
